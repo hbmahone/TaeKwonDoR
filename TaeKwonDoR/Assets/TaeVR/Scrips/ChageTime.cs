5f600e8b6ff4db988fc69540e4c6723609fd8537
Henry Mahoney <hbmahone@asu.edu>
2017-03-12 07:50:35 -0700

    TaeVR

---
+++ TaeKwonDoR/Assets/TaeVR/Scrips/ChageTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChageTime : MonoBehaviour {
+
+    public GameObject timer;
+    //public GameObject SceneManager;
+	// Use this for initialization
+	void Start () {
+
+        timer.gameObject.GetComponent<TextMesh>().text = "Hello";
+    }
+
+    float timeLeft = 15.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft > 0)
+        {
+            timer.gameObject.GetComponent<TextMesh>().text = timeLeft.ToString().Substring(0, 4);
+           gameObject.GetComponent<KickGabTargetGenreation>().isOn = false;        
+                }
+    }
+}
